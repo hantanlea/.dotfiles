027a8e46cac25d818956fcbac063097155eb80f4
Nathan <nathanaelpthomas@gmail.com>
2020-08-22 20:37:39 +0100

    Added dotfile import script

---
+++ import.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Any dotfiles in home directory present in repo will be renamed with .old then new file created and symlinked to file in repo
+
+dir="$(pwd)" 
+for f in .[^.]* 
+do
+   echo "Moving $HOME/$f to $HOME/$f.old"
+   mv "$HOME/$f" "$HOME/$f.old"
+   echo "Importing $f"
+   ln -s "$dir/$f" "$HOME/$f"
+done
